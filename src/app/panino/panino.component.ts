064bbcf0d94186b7a81833d099860f76eb8c1bf2
PsykeDady <psdady@msn.com>
2021-10-27 09:08:46 +0200

    strutturato progetto

---
+++ src/app/panino/panino.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panino',
+  templateUrl: './panino.component.html',
+  styleUrls: ['./panino.component.css']
+})
+export class PaninoComponent implements OnInit {
+
+  collapsed=true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
