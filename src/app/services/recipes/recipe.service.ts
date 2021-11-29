3a8f57b9754de1bf535864e5bfea07c66fcc672d
PsykeDady <psdady@msn.com>
2021-11-29 15:54:10 +0100

    Added services to add ingredients

--- src/app/services/recipes/recipe.service.ts
+++ src/app/services/recipes/recipe.service.ts
@@ -7,7 +7,13 @@ export class RecipeService{
 	selectedRecipe_event: EventEmitter<number> = new EventEmitter<number>();
 
 	private _recipes : Recipe [] = [
-		new Recipe('Pasta al sugo','Penne condite con sugo','https://static.cookist.it/wp-content/uploads/sites/21/2017/10/penne-al-sugo-di-pomodoro.jpg'),
+		new Recipe("Niente","prendi un piatto vuoto, servilo","https://unniente.com/wp-content/uploads/2018/12/unniente.jpg"),
+		new Recipe('Pasta al sugo','Penne condite con sugo','https://static.cookist.it/wp-content/uploads/sites/21/2017/10/penne-al-sugo-di-pomodoro.jpg',
+			new Ingredient("penne",500),
+			new Ingredient("passata di pomodoro",0.5),
+			new Ingredient("cipolla",1),
+			new Ingredient("basilico",1),
+		),
 		new Recipe('Lasagne','Lasagne con ragu bolognese','https://www.cucchiaio.it/content/cucchiaio/it/ricette/2019/11/lasagne-senza-besciamella/jcr:content/header-par/image-single.img10.jpg/1580462408244.jpg',
 			new Ingredient("Lasagna",1),
 			new Ingredient("Carne Macinata",500),
