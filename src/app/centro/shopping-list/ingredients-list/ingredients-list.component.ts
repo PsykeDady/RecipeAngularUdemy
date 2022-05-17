207dfd0cf9d48255044c25d54653c6ad691318c7
PsykeDady <psdady@msn.com>
2022-05-17 12:22:23 +0200

    scroll on ing-edit

--- src/app/centro/shopping-list/ingredients-list/ingredients-list.component.ts
+++ src/app/centro/shopping-list/ingredients-list/ingredients-list.component.ts
@@ -17,6 +17,7 @@ export class IngredientsListComponent {
 	sendEdit(ingredient:Ingredient){
 		this.editIngredientService.ingredient=new Ingredient(ingredient.name,ingredient.qta)
 		this.editIngredientService.editMode.emit();
+		document.getElementById("centro-shopping-list").scrollTo(0,0)
 	}
 
 }
