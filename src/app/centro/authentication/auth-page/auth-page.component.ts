962842c206a0ea9159b196f608eb386f9a5d9cb4
PsykeDady <psdady@msn.com>
2022-07-11 14:58:52 +0200

    modificato:             src/app/centro/authentication/auth-page/auth-page.component.ts

--- src/app/centro/authentication/auth-page/auth-page.component.ts
+++ src/app/centro/authentication/auth-page/auth-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
